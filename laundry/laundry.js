707d1a01b2ecbbef322e2f860158cd42586f1951
WinterCricket <hodhodhod2003@gmail.com>
2017-02-14 19:19:10 -0600

    make laundryList add objects instead of items, intro boolean

--- laundry/laundry.js
+++ laundry/laundry.js
@@ -1,99 +1,36 @@
 
-//variable array to be filled, to store item names
-//var laundry = ["yellow socks", "old dance shoes", "new dance shoes", "straw hat", "long pants"];
-
-//add to array variable
-/*laundry.push()
-
-//to update item in array
-laundry[] = ''
-
-//to delete item(s) from array, index position and number of items
-laundry.splice(,)
-
-//to view
-console.log(laundry)
-
-//function analogy
-function makeSandwichWith(filler){
-	Place slice 1 bread on plate;
-	Add filler to slice 1;
-	Place  slice 2 on top ;
-}
-
-makeSandwichWith("Salmon")
 
 
-//my function for todos display
-function viewLaundry(){
-	console.log('My damned laundry list: ', laundry);
-}
-viewLaundry()
 
-//function to add item to laundry
-function addItem() {
-	laundry.push('new item');
-}
-addItem()
 
-//now put viewLaundry function inside addItem
-function addItem() {
-	laundry.push('new item');
-	viewLaundry();
-}
-addItem()
-//now addItem will display list with item added
 
-//now make it so you can add new items to list with new variable
-//requirements met: display, add item
-function addItem(garment) {
-	laundry.push(garment);
-	viewLaundry();
-}
-addItem("code mail socks")
-//another function, this one changing and showing change to item
-function changeItem(position, newValue) {
-	laundry[position] = newValue;
-	viewLaundry();
-}
-changeItem(3, 'yellow socks')
 
-//now the delete function
-function removeItem(position) {
-	laundry.splice(position, 1);
-	viewLaundry();
-}
-removeItem(2)
-
-//object with a function as a property
-var me = {
-	name: 'Todd',
-	sayName: function() {
-		console.log(this.name);
-	}
-}*/
-
-//add functions into the object--use this.property,
+//add functions into the object--use this.property, then
+//make laundryList add objects instead of items, intro boolean
 var laundryList = {
-	laundry: ["yellow socks", "old dance shoes", "new dance shoes", "straw hat", "long pants"],
+	laundry: [],
 	viewLaundry: function(){
 		console.log('My damned laundry list: ', this.laundry);
 	},
-	addItem: function(garment){
-		this.laundry.push(garment)
+	addObj: function(garmentText){
+		this.laundry.push({
+			garmentText: garmentText,
+			completed: false
+		})
 		this.viewLaundry();
 	},
-	changeItem: function(pos, newVal) {
-		this.laundry[pos] = newVal;
+	changeObj: function(pos, garmentText) {
+		this.laundry[pos].garmentText = garmentText;
 		this.viewLaundry();
 	},
-	removeItem: function (pos) {
+	removeObj: function (pos) {
 		this.laundry.splice(pos, 1);
+		this.viewLaundry();
+	},
+	toggleCompleted: function(pos){
+		var garment = this.laundry[pos];
+		garment.completed = !garment.completed;
 		this.viewLaundry();
 	}
 };
 
-// removeItem: function (pos) {
-// 	this.laundry.splice(pos, 1);
-// 	viewLaundry();
-// }

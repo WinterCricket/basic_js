c5dfa78632876178351a0972fbe037a8a79df4e2
WinterCricket <hodhodhod2003@gmail.com>
2017-02-22 16:17:30 -0600

    Refactor by creating second object

--- laundry/laundry.js
+++ laundry/laundry.js
@@ -3,27 +3,29 @@ var laundryList = {
 	laundry: [],
 	viewLaundry: function(){
 		if(this.laundry.length === 0){
-			console.log("My laundry list is empty!");
-		} else {
-			console.log("Here's my damned laundry list: ");
+			
+			console.log("Your bloody laundry list is bloody empty!");
+		}else{
+			console.log("Here's your stinking list, fool!");
 			for(var i = 0; i < this.laundry.length; i++){
-				if(this.laundry[i].completed === true){
-					console.log("(x)", this.laundry[i].laundryText);
+				if(this.laundry[i].completed == true){
+					console.log('(x)', this.laundry[i].laundryText);
 				}else{
-					console.log("()", this.laundry[i].laundryText);
+					console.log('()', this.laundry[i].laundryText);
 				}
 			}
 		}
+		
 	},
-	addObj: function(garmentText){
+	addObj: function(garbText){
 		this.laundry.push({
-			laundryText: garmentText,
+			laundryText: garbText,
 			completed: false
 		});
 		this.viewLaundry();
 	},
-	changeObj: function(pos, garmentText){
-		this.laundry[pos].laundryText = garmentText;
+	changeObj: function(pos, garbText){
+		this.laundry[pos].laundryText = garbText;
 		this.viewLaundry();
 	},
 	removeObj: function(pos){
@@ -38,35 +40,37 @@ var laundryList = {
 	toggleAll: function(){
 		var totalLaundry = this.laundry.length;
 		var completedLaundry = 0;
-
 		for(var i = 0; i < totalLaundry; i++){
 			if(this.laundry[i].completed === true){
 				completedLaundry++;
 			}
 		}
-
 		if(completedLaundry === totalLaundry){
 			for(var i = 0; i < totalLaundry; i++){
-				this.laundry[i].completed = false;
-            	}
-			} else{
-				for(var i = 0; i < totalLaundry; i++){
-					this.laundry[i].completed = true;
-				}
-				
+			this.laundry[i].completed = false;	
 			}
-		 
+			
+		}else{
+			for(var i = 0; i < totalLaundry; i++){
+				this.laundry[i].completed = true;
+			}
+			
+		}
 		this.viewLaundry();
 	}
+	
+	
 };
 
-//access to view laundry button and toggle button
-var viewLaundryButton = document.getElementById("viewLaundryButton"); 
-var toggleAllButton = document.getElementById("toggleAllButton");
-// click to run viewLaundry and toggleAll methods 
-viewLaundryButton.addEventListener('click', function(){
-	laundryList.viewLaundry();
-});
-toggleAllButton.addEventListener('click', function(){
-	laundryList.toggleAll();
-}); 
+
+var handlers = {
+	viewLaundry: function(){
+		laundryList.viewLaundry();
+	},
+	toggleAll: function(){
+		laundryList.toggleAll();
+	},
+	addObj: function(){
+		laundryList.addOb();
+	}
+};

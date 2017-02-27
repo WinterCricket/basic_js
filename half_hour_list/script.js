9a6fbe1ac0b0b81acbbb2d02fe4a66c839f7d965
WinterCricket <hodhodhod2003@gmail.com>
2017-02-27 13:54:22 -0600

    Make created items like other items

--- half_hour_list/script.js
+++ half_hour_list/script.js
@@ -8,17 +8,14 @@ var listItems = document.getElementById("our-list").getElementsByTagName("li");
 // }
 ourList.addEventListener('click', activateItem);
 
-function activateItem(){
-	if() {
-		
+function activateItem(e){
+	if(e.target.nodeName === "LI") {
+		ourHeadline.innerHTML = e.target.innerHTML;
+		for(var i = 0; i < e.target.parentNode.children.length; i++){
+			e.target.parentNode.children[i].classList.remove('active');
+		}
+			e.target.classList.add("active");
 	}
-
-
-	ourHeadline.innerHTML = this.innerHTML;
-	for(var i = 0; i < listItems.length; i++){
-	listItems[i].classList.remove('active');
-}
-	this.classList.add("active");
 
 
 }

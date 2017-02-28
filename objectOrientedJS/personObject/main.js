6dd6b260f25046cebc221f5b6b0377f40a1f857c
WinterCricket <hodhodhod2003@gmail.com>
2017-02-27 20:46:46 -0600

    Build constructor

--- objectOrientedJS/personObject/main.js
+++ objectOrientedJS/personObject/main.js
@@ -3,10 +3,19 @@ function person(name, music){
 }
 
 
-
+//object:
 var ophelia = {
 	name: "Ophelia",
 	music: "Walk, by Tycho"
 };
 
 person(ophelia.name, ophelia.music);
+
+//class: blueprint for objects
+function Person(){
+	this.salut = function(){
+		console.log("I'm " + name +". My choice in music is " + music +".")
+	}
+}
+
+var ophelia = new Person();

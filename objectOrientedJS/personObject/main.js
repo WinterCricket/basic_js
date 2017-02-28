6a78dbf856cfde60b576d2bbb06bb8a172f94658
WinterCricket <hodhodhod2003@gmail.com>
2017-02-27 21:36:22 -0600

    complete class and objects

--- objectOrientedJS/personObject/main.js
+++ objectOrientedJS/personObject/main.js
@@ -1,21 +1,26 @@
-function person(name, music){
-	console.log("I am " + name +". My choice in music is " + music +".");
-}
 
 
 //object:
-var ophelia = {
-	name: "Ophelia",
-	music: "Walk, by Tycho"
-};
+// var ophelia = {
+// 	name: "Ophelia",
+// 	music: "Walk, by Tycho"
+// };
 
-person(ophelia.name, ophelia.music);
+
 
 //class: blueprint for objects
-function Person(){
-	this.salut = function(){
-		console.log("I'm " + name +". My choice in music is " + music +".")
+function Person(fullName, composer){
+	this.name = fullName;
+	this.music = composer;
+	this.greet = function(){
+		console.log("I'm "+ this.name + ", and I listen to " + this.music +".");
 	}
 }
 
-var ophelia = new Person();
+
+var ophelia = new Person("Ophelia of Polonius", "Tycho");
+ophelia.greet();
+
+var hamlet = new Person("Hamlet of Hamlet", "Air");
+hamlet.greet();
+
